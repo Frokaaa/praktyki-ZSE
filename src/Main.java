01ff6b9c77d5d3208047ae1ceca124ddb3885115
Frokaaa <mariusz22.76@tlen.pl>
2020-10-28 10:53:52 +0100

    pierwszy commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 12;
+        int b = 15;
+        double sum = a + b;
+
+
+
+
+        System.out.println("wynik = " + sum);
+
+
+    }
+}
