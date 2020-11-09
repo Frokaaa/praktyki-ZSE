8ab25ecfa004c2373ddac4d0cc6dea3d625240ce
Frokaaa <mariusz22.76@tlen.pl>
2020-11-09 02:36:31 +0100

    trzeci commit

---
+++ src/Ptaszek/Main.java
@@ -0,0 +1,11 @@
+package Ptaszek;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ptak ptak1 = new Ptak("czerwony", "gil", "łuszczakowate", true);
+        Ptak ptak2 = new Ptak("Żółty","dzwoniec","łuszczakowate", true);
+
+    }
+
+}
---
+++ src/Ptaszek/Ptak.java
@@ -0,0 +1,21 @@
+package Ptaszek;
+
+public class Ptak {
+
+    String kolor;
+    String gatunek;
+    String rodzina;
+    boolean czyrobigniazda;
+
+
+
+    Ptak(String kolor, String gatunek, String rodzina, boolean czyrobigniazda){
+        this.kolor = kolor;
+        this.gatunek = gatunek;
+        this.rodzina = rodzina;
+        this.czyrobigniazda = czyrobigniazda;
+    }
+
+}
+
+
